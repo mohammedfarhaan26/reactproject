bdf10db946bfb23fea18f80c078774747e5fa584
mohammedfarhaan <mohammed.farhan@liferay.com>
2023-05-11 14:26:54 +0530

    first commit

---
+++ robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'stylixfarhaan',
+        username: 'stylix',
+        email: 'stylixfarhaan45@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'santhosh',
+        username: 'santhosh',
+        email: 'santhosh@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'parveen',
+        username: 'parveen',
+        email: 'parveen@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'sivamani',
+        username: 'sivamani',
+        email: 'sivamani@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'rakesh',
+        username: 'rakesh',
+        email: 'rakesh@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'rujith',
+        username: 'rujith',
+        email: 'rujith@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'sharan',
+        username: 'sharan',
+        email: 'sharan@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'pravin st',
+        username: 'pravin',
+        email: 'pravin@gmail.com'
+    }
+]
